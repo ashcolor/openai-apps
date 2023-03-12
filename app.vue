fb30c48f67b1c43f0b14a549b669d6dd2bf8915e
ashcolor <ashcolor27@gmail.com>
2023-03-12 17:28:05 +0900

    add: init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+    htmlAttrs: { "data-theme": "garden" },
+});
+</script>
+
+<template>
+    <div class="h-screen flex flex-col">
+        <CustomHeader />
+        <div class="grow container mx-auto min-h-0">
+            <NuxtPage />
+        </div>
+        <CustomFooter />
+    </div>
+</template>
