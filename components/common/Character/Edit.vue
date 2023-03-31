445b5ecc5db2fe43a4d3a41543603f21415ae2b2
ashcolor <ashcolor27@gmail.com>
2023-03-31 09:44:31 +0900

    fix: レイアウトが崩れる問題の修正

--- components/common/Character/Edit.vue
+++ components/common/Character/Edit.vue
@@ -32,35 +32,33 @@ const onClickSave = async () => {
 </script>
 
 <template>
-    <div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Profile Picture</span>
-            </label>
-            <AvatarImgWithFileUpload v-model:src="avatarSrc"></AvatarImgWithFileUpload>
-        </div>
-        <div class="form-control grow flex flex-col">
-            <label class="label">
-                <span class="label-text">名前</span>
-            </label>
-            <input
-                v-model="name"
-                type="text"
-                placeholder="Type here"
-                class="input input-sm input-bordered"
-            />
-            <label class="label">
-                <span class="label-text">プロンプト</span>
-            </label>
-            <textarea
-                v-model="prompt"
-                type="text"
-                placeholder="Type here"
-                class="textarea textarea-bordered grow"
-            ></textarea>
-            <button type="button" class="btn btn-outlinew-full" @click="onClickSave()">保存</button>
-            <div class="divider"></div>
-            <CharacterDeleteButton></CharacterDeleteButton>
-        </div>
+    <div class="form-control">
+        <label class="label">
+            <span class="label-text">Profile Picture</span>
+        </label>
+        <AvatarImgWithFileUpload v-model:src="avatarSrc"></AvatarImgWithFileUpload>
+    </div>
+    <div class="form-control grow flex flex-col">
+        <label class="label">
+            <span class="label-text">名前</span>
+        </label>
+        <input
+            v-model="name"
+            type="text"
+            placeholder="Type here"
+            class="input input-sm input-bordered"
+        />
+        <label class="label">
+            <span class="label-text">プロンプト</span>
+        </label>
+        <textarea
+            v-model="prompt"
+            type="text"
+            placeholder="Type here"
+            class="textarea textarea-bordered grow"
+        ></textarea>
+        <button type="button" class="btn btn-outlinew-full" @click="onClickSave()">保存</button>
+        <div class="divider"></div>
+        <CharacterDeleteButton></CharacterDeleteButton>
     </div>
 </template>
