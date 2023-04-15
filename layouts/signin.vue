78eda74aa2713cf2f7ae51a25262f86b92886177
ashcolor <ashcolor27@gmail.com>
2023-04-15 16:34:57 +0900

    chg: imgをassetsに移動

--- layouts/signin.vue
+++ layouts/signin.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="h-screen flex flex-col">
         <div
-            class="min-h-0 bg-[linear-gradient(to_bottom,rgba(0,0,100,.5),rgba(0,0,0,09)),url('/img/bg.jpg')] bg-no-repeat bg-cover bg-center"
+            class="min-h-0 bg-[linear-gradient(to_bottom,rgba(0,0,100,.5),rgba(0,0,0,09)),url('/assets/img/bg.jpg')] bg-no-repeat bg-cover bg-center"
         >
             <slot></slot>
         </div>
