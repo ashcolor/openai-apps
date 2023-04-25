1390313544d835ca35913a1596e880796ac0d1e3
ashcolor <ashcolor27@gmail.com>
2023-04-25 12:42:40 +0900

    chg: キャラクターが未選択の場合は選択画面を出す

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@ const systemStore = useSystemStore();
 const { isCharacterEditing } = storeToRefs(systemStore);
 
 const chatStore = useChatStore();
-const { selectedChatId } = storeToRefs(chatStore);
+const { chat, selectedChatId } = storeToRefs(chatStore);
 
 const characterStore = useCharacterStore();
 const { character } = storeToRefs(characterStore);
@@ -23,54 +23,64 @@ const { character } = storeToRefs(characterStore);
         <div class="shrink-0 w-72">
             <ChatList></ChatList>
         </div>
-        <div v-show="selectedChatId" class="grow bg-base-200 flex flex-col">
-            <div class="shrink-0 w-full p-2 flex flex-row justify-center gap-4">
-                <CharacterAvatar></CharacterAvatar>
-                <CharacterNameHeader></CharacterNameHeader>
-                <button
-                    v-show="isCharacterEditing"
-                    class="btn btn-square"
-                    @click="isCharacterEditing = false"
-                >
-                    <Icon name="ic:baseline-chat" width="24" height="24" />
-                </button>
-                <button
-                    v-show="!isCharacterEditing"
-                    class="btn btn-square"
-                    @click="isCharacterEditing = true"
-                >
-                    <Icon name="ic:baseline-person" width="24" height="24" />
-                </button>
-                <MessageDeleteAllButton></MessageDeleteAllButton>
-            </div>
-            <div class="divider m-0"></div>
-            <div v-show="!isCharacterEditing" class="grow min-h-0 flex flex-col">
-                <div class="grow min-h-0 p-4">
-                    <ChatDisplay></ChatDisplay>
-                </div>
-                <div class="divider h-0 m-0"></div>
-                <div class="p-4">
-                    <div class="flex flex-col gap-2">
-                        <MessageTemplateButtonGroup></MessageTemplateButtonGroup>
-                        <MessageForm></MessageForm>
-                    </div>
-                </div>
-            </div>
-            <div v-show="isCharacterEditing" class="grow min-h-0 p-4">
-                <div class="h-full flex flex-col">
-                    <div class="flex flex-row flex-wrap gap-2 justify-center place-items-center">
-                        <CharacterSelect></CharacterSelect>
-                        <CharacterAddButton></CharacterAddButton>
-                    </div>
-                    <div class="divider m-0"></div>
-                    <div class="grow flex flex-row gap-8">
-                        <CharacterEdit v-show="character" class="grow"></CharacterEdit>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div v-show="!selectedChatId" class="grow bg-base-200 grid place-items-center">
+        <div v-if="!selectedChatId" class="grow bg-base-200 grid place-items-center">
             <ChatNewButton></ChatNewButton>
+        </div>
+        <div v-else class="grow bg-base-200 flex flex-col">
+            <div
+                v-if="!chat?.character_id"
+                class="grow bg-base-200 flex flex-col place-content-center place-items-center gap-8"
+            >
+                <CharacterSelectCardGroup></CharacterSelectCardGroup>
+            </div>
+            <template v-else>
+                <div class="shrink-0 w-full p-2 flex flex-row justify-center gap-4">
+                    <CharacterAvatar></CharacterAvatar>
+                    <CharacterNameHeader></CharacterNameHeader>
+                    <button
+                        v-show="isCharacterEditing"
+                        class="btn btn-square"
+                        @click="isCharacterEditing = false"
+                    >
+                        <Icon name="ic:baseline-chat" width="24" height="24" />
+                    </button>
+                    <button
+                        v-show="!isCharacterEditing"
+                        class="btn btn-square"
+                        @click="isCharacterEditing = true"
+                    >
+                        <Icon name="ic:baseline-person" width="24" height="24" />
+                    </button>
+                    <MessageDeleteAllButton></MessageDeleteAllButton>
+                </div>
+                <div class="divider m-0"></div>
+                <div v-show="!isCharacterEditing" class="grow min-h-0 flex flex-col">
+                    <div class="grow min-h-0 p-4">
+                        <ChatDisplay></ChatDisplay>
+                    </div>
+                    <div class="divider h-0 m-0"></div>
+                    <div class="p-4">
+                        <div class="flex flex-col gap-2">
+                            <MessageTemplateButtonGroup></MessageTemplateButtonGroup>
+                            <MessageForm></MessageForm>
+                        </div>
+                    </div>
+                </div>
+                <div v-show="isCharacterEditing" class="grow min-h-0 p-4">
+                    <div class="h-full flex flex-col">
+                        <div
+                            class="flex flex-row flex-wrap gap-2 justify-center place-items-center"
+                        >
+                            <CharacterSelect></CharacterSelect>
+                            <CharacterAddButton></CharacterAddButton>
+                        </div>
+                        <div class="divider m-0"></div>
+                        <div class="grow flex flex-row gap-8">
+                            <CharacterEdit v-show="character" class="grow"></CharacterEdit>
+                        </div>
+                    </div>
+                </div>
+            </template>
         </div>
     </div>
     <SettingsModal></SettingsModal>
