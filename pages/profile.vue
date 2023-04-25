e4ce5889f218b0be5c0ef8422afde7ebdb8071a7
ashcolor <ashcolor27@gmail.com>
2023-04-26 00:53:06 +0900

    chg: ヘッダー修正

--- pages/profile.vue
+++ pages/profile.vue
@@ -7,7 +7,7 @@ definePageMeta({
 <template>
     <div class="h-full px-8 py-4 space-y-4">
         <div>
-            <h2 class="text-2xl font-bold border-b-4 border-secondary py-4">設定</h2>
+            <h2 class="text-2xl font-bold border-b-4 border-secondary py-4">プロフィール</h2>
         </div>
         <ProfileAccount></ProfileAccount>
     </div>
