3dda5b601ff387f97b3795884c1a65eb20f2c306
ashcolor <ashcolor27@gmail.com>
2023-04-26 16:04:36 +0900

    del: 不要なコードの削除

--- stores/useSystemStore.ts
+++ stores/useSystemStore.ts
@@ -4,12 +4,9 @@ export const useSystemStore = defineStore("system", () => {
     const openAiApiKey = useLocalStorage("open-ai-api-key", "");
     const userAvatarSrc = useLocalStorage("user-avatar-src", "");
 
-    const isCharacterEditing = ref<boolean>(false);
-
     return {
         // @https://pinia.vuejs.org/cookbook/composables.html#ssr
         openAiApiKey: skipHydrate(openAiApiKey),
         userAvatarSrc: skipHydrate(userAvatarSrc),
-        isCharacterEditing,
     };
 });
