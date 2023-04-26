76a34a3c179c55d8de2ac09e4f8f9e43f7ad6fc4
ashcolor <ashcolor27@gmail.com>
2023-04-26 17:08:28 +0900

    fix: OpenAI APIキー未登録オーバーレイが常に表示されていた問題の修正

--- stores/useProfileStore.ts
+++ stores/useProfileStore.ts
@@ -2,13 +2,7 @@ import { defineStore, skipHydrate, storeToRefs } from "pinia";
 import { profiles } from "@prisma/client";
 
 export const useProfileStore = defineStore("profile", () => {
-    const {
-        data: profile,
-        execute,
-        refresh,
-    } = useFetch<profiles>("/api/profiles", {
-        immediate: false,
-    });
+    const { data: profile, execute, refresh } = useFetch<profiles>("/api/profiles");
 
     const patchProfile = async (body: Object) => {
         const savedProfile = await $fetch("/api/profiles", {
